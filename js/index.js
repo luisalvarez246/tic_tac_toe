06f11b55b28bdf0d6e6e1812c48e955ed48fc10f
Lualvare <luisalvarez1@correo.unimet.edu.ve>
2023-07-28 17:19:43 +0200

    added error message to view, fixed tie messages

--- js/index.js
+++ js/index.js
@@ -5,11 +5,12 @@ export const	tictac = new TicTacToe();
 export const	gameReset = () =>
 {
 	let	cell;
-	let	message;
+	let	message = document.getElementById('winner');
+	let	error = document.getElementById('error');
 	
 	tictac.gameReset();
-	message = document.getElementById('winner');
 	message.innerText = '';
+	error.innerText = '';
 	for (let i = 0; i < 3; i++)
 	{
 		for (let j = 0; j < 3; j++)
@@ -24,6 +25,7 @@ export const	ftClickListener = () =>
 {
 	const	board = document.getElementById('gameboard');
 	const	message = document.getElementById('winner');
+	const	error = document.getElementById('error');
 
 	board.addEventListener('click', (event)=>
 	{
@@ -33,9 +35,19 @@ export const	ftClickListener = () =>
 
 		tictac.setPlay(row, col);
 		if (tictac.error === '')
+		{
 			event.target.innerText = tictac.getPlay();
-		if (tictac.message !== '')
+			error.innerText = "";
+		}
+		else
+		{
+			error.innerText = tictac.error;
+			tictac.error = '';
+		}
+		if (tictac.message)
+		{
 			message.innerText = tictac.message;
+		}
 	})
 }
 
